943626e0922844b97f6f2077ee28b763c1d4820e
Michelle Rios <brendamichelle995@gmail.com>
2021-04-30 09:43:44 -0400

    add DQ solutions

--- review-question-1.rb
+++ review-question-1.rb
@@ -1,69 +1,101 @@
 ## QUESTION 1
+require "pry"
 
-pokemon = [
-  {
-    "id": 1,
-    "name": "bulbasaur",
-    "base_experience": 64,
-    "height": 7,
-    "is_default": true,
-    "order": 1,
-    "weight": 69,
-    "abilities": [
+def pokemon
+  [
+    {
+      "id": 1,
+      "name": "bulbasaur",
+      "base_experience": 64,
+      "height": 7,
+      "is_default": true,
+      "order": 1,
+      "weight": 69,
+      "abilities": [
         {
-            "is_hidden": true,
-            "slot": 3,
-            "ability": {
-                "name": "chlorophyll",
-                "url": "http://pokeapi.co/api/v2/ability/34/"
-            }
-        }
-    ]
-},
-{
-  "id": 3,
-  "name": "venesaur",
-  "base_experience": 50,
-  "height": 10,
-  "is_default": true,
-  "order": 1,
-  "weight": 90,
-  "abilities": [
-      {
           "is_hidden": true,
           "slot": 3,
           "ability": {
-              "name": "fire",
-              "url": "http://pokeapi.co/api/v2/ability/38/"
-          }
-      }
-  ]
-},
-{
-  "id": 2,
-  "name": "pikachu",
-  "base_experience": 60,
-  "height": 4,
-  "is_default": true,
-  "order": 1,
-  "weight": 37,
-  "abilities": [
-      {
+            "name": "chlorophyll",
+            "url": "http://pokeapi.co/api/v2/ability/34/",
+          },
+        },
+      ],
+    },
+    {
+      "id": 3,
+      "name": "venesaur",
+      "base_experience": 50,
+      "height": 10,
+      "is_default": true,
+      "order": 1,
+      "weight": 90,
+      "abilities": [
+        {
           "is_hidden": true,
           "slot": 3,
           "ability": {
-              "name": "lightning",
-              "url": "http://pokeapi.co/api/v2/ability/30/"
-          }
-      }
+            "name": "fire",
+            "url": "http://pokeapi.co/api/v2/ability/38/",
+          },
+        },
+      ],
+    },
+    {
+      "id": 2,
+      "name": "pikachu",
+      "base_experience": 60,
+      "height": 4,
+      "is_default": true,
+      "order": 1,
+      "weight": 37,
+      "abilities": [
+        {
+          "is_hidden": true,
+          "slot": 3,
+          "ability": {
+            "name": "lightning",
+            "url": "http://pokeapi.co/api/v2/ability/30/",
+          },
+        },
+      ],
+    },
   ]
-}
-]
-
+end
 
 # How would you get the url for Bulbasaur's ability?
+def bulbasaur_url
+  pokemon[0][:abilities][0][:ability][:url]
+end
+
 # How would you return the first pokemon with base experience over 40?
+def exp_over_40
+  pokemon.find do |pokemon_instance|
+    pokemon_instance[:base_experience] > 40
+  end
+end
+
 # How would you return ALL OF THE pokemon with base experience over 40? (Gotta catch em all)
+def all_with_exp_over_40
+  pokemon.select do |pokemon_instance|
+    pokemon_instance[:base_experience] > 40
+  end
+end
+
 # How would you return an array of all of the pokemon's names?
+def all_with_exp_over_40
+  pokemon.map do |pokemon_instance|
+    pokemon_instance[:name]
+  end
+end
+
 # How would you determine whether or not the pokemon array contained any pokemon with a weight greater than 60?
 #  whatever method you use should return true if there are any such pokemon, false if not.
+def over_60_weight?
+  pokemon.any? do |pokemon_instance|
+    pokemon_instance[:weight] > 60
+  end
+end
+
+
+binding.pry
--- review-question-2.rb
+++ review-question-2.rb
@@ -1,27 +1,42 @@
 # Finish the implementation of the Car class so it has the functionality described below
+require 'pry'
 
 class Car
+  attr_reader :make, :model
+  @@all = []
 
+  def initialize(args)
+    @make = args[:make]
+    @model = args[:model]
 
+    @@all << self
+  end
 
+  def self.all
+    @@all
+  end
+
+  def self.drive
+    "VROOOOOOOOOOOOM!"
+  end
 end
 
-volvo_lightning = Car.new("Volvo", "Lightning")
-yugo = Car.new("Zastava", "Yugo")
-lada = Car.new("AvtoVAZ", "Lada")
+# volvo_lightning = Car.new("Volvo", "Lightning")
+# yugo = Car.new("Zastava", "Yugo")
+# lada = Car.new("AvtoVAZ", "Lada")
 
-volvo_lightning.make
-#=> "Volvo"
-volvo_lightning.model
-#=> "Lightning"
+# volvo_lightning.make
+# #=> "Volvo"
+# volvo_lightning.model
+# #=> "Lightning"
 
-Car.drive
-# => "VROOOOOOOOOOOOM!"
+# Car.drive
+# # => "VROOOOOOOOOOOOM!"
 
-Car.all
+# Car.all
 #=> [#<Car:0x00007fae28930f20>, #<Car:0x00007fae28923370>, #<Car:0x00007fae2891ae78>]
 
-BONUS:
+# BONUS:
 
 volvo_lightning = Car.new(make: "Volvo", model: "Lightning")
 
@@ -29,3 +44,5 @@ volvo_lightning.make
 #=> "Volvo"
 volvo_lightning.model
 #=> "Lightning"
+
+binding.pry
